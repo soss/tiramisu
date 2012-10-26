b0ded28f6fc6bd3263c78e9fab43afc06fb6f1e6
Jordan Scales <scalesjordan@gmail.com>
2012-10-25 21:34:11 -0400

    lets roll

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tiramisu::Application.config.secret_token = '6c252cc423b988674e4f76738e80bb16e5bca8bb1f0598da414b9f7edfedaf99a41d2a0ef1f892874a3834f6ffe5045c34db633a3bc65dc1f94b8f269f151fbc'
