92b5b9f929e51bb31cf6c98fe5738c71ce472ca2
Jordan Scales <scalesjordan@gmail.com>
2013-01-27 11:19:57 -0500

    added dependencies on 1-M relations

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,6 @@ class Project < ActiveRecord::Base
   attr_accessible :description, :language, :long_description, :name
   # we don't want to allow mass-assignment for accepted, user_id, etc.
 
-  has_many :votes
+  has_many :votes, :dependent => :destroy
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ActiveRecord::Base
   validates_presence_of :email, :username
   validates_uniqueness_of :email, :username
 
-  has_many :projects
+  has_many :projects, :dependent => :destroy
 end
