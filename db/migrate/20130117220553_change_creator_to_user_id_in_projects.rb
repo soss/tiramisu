f43a0b5c0dc3a33fa5fea927f1fd84ef5339d470
Jordan Scales <scalesjordan@gmail.com>
2013-01-30 00:23:36 -0500

    removed rogue migration

--- db/migrate/20130117220553_change_creator_to_user_id_in_projects.rb
+++
@@ -1,9 +0,0 @@
-class ChangeCreatorToUserIdInProjects < ActiveRecord::Migration
-  def up
-    rename_column :projects, :creator, :user_id
-  end
-
-  def down
-    rename_column :projects, :user_id, :creator
-  end
-end
