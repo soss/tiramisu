66d26a53d48e77b8a7b801e9c1cac0c7957d9150
Jordan Scales <scalesjordan@gmail.com>
2013-01-29 21:33:45 -0500

    user profile view and some helpers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def update
     @user = User.find_by_username(params[:username])
     if current_user == @user
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  # renders markdown, escaping premature HTML code before-hand
+  def markdown(text)
+    text = text.gsub(/</,'&lt;')
+               .gsub(/>/,'&gt;')
+    Redcarpet.new(text).to_html.html_safe
+  end
+
+  # gravatar
+  def gravatar_url(user, height = 64)
+    default_url = "#{root_url}assets/default_user.png"
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{height}&d=#{CGI.escape(default_url)}"
+  end
+
 end
