b18ff652f410f31927ead35cb79db200a998a807
Jordan Scales <scalesjordan@gmail.com>
2013-01-30 00:47:50 -0500

    removed default gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,9 +9,8 @@ module ApplicationHelper
 
   # gravatar
   def gravatar_url(user, height = 64)
-    default_url = "#{root_url}assets/default_user.png"
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{height}&d=#{CGI.escape(default_url)}"
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{height}"
   end
 
 end
