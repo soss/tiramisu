25130beb78912b3da6ec69658586a333e5b55f54
Jordan Scales <scalesjordan@gmail.com>
2013-02-06 18:08:50 -0500

    fixed bug that occured when user entered incorrect credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
     if user
       redirect_back_or_to root_url, :notice => "Logged in!"
     else
-      flash.now.alert = "Email or password was invalid."
+      flash.now.alert = "Username or password was invalid."
+      render :new
     end
   end
 
