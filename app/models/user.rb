2bcb254fb363bca224c5742d41078d29ed84b6e0
Jordan Scales <scalesjordan@gmail.com>
2013-02-08 16:43:08 -0500

    default scope for users - earliest first

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,17 @@ class User < ActiveRecord::Base
   validates_presence_of :email
   validates_uniqueness_of :email, :username
 
-	validates :username, :length => { :in => 3..20 }
-	validates :password, :length => { :in => 5..32 }
+  validates :username, :length => { :in => 3..20 }
+  validates :password, :length => { :in => 5..32 }
 
-	validates_format_of :username, :password, :with => /\A\w*\Z/
-	validates_format_of :email, :with => /\A[\w\+\.]+@[\w\-]+\.\w+\Z/
+  validates_format_of :username, :password, :with => /\A\w*\Z/
+  validates_format_of :email, :with => /\A[\w\+\.]+@[\w\-]+\.\w+\Z/
 
   has_many :projects, :dependent => :destroy
   has_many :votes, :dependent => :destroy
   has_many :pledges, :dependent => :destroy
   has_many :comments, :dependent => :destroy
 
-	scope :admins, where(:role => 1)
+  scope :admins, where(:role => 1)
+  default_scope order('created_at ASC')
 end
