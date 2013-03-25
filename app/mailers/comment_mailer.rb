a4182f5ccf2f3b27c4fa2052f1f1baf80a023793
Jordan Scales <scalesjordan@gmail.com>
2013-03-25 14:01:30 -0400

    some reformatting on comment_mailer, tabs -> spaces

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,12 +1,15 @@
 class CommentMailer < ActionMailer::Base
-	default from: "tiramisuGitHub@gmail.com"
+  default from: "tiramisuGitHub@gmail.com"
+  helper :application
 
-	helper :application
-	def comment_email(comment)
-		@comment = comment
-		user = comment.project.user
-		#email = comment.project.user.(:email) #project lead email
-		@commenter = comment.user
-		mail(:to => user.email, :subject => "New comment on (#{@comment.project.name})")
-	end
+  def comment_email(comment)
+    @comment = comment
+    @commenter = @comment.user
+    @projlead  = @comment.project.user
+
+    # no need to send the project lead his own comment
+    unless @commenter == @projlead
+      mail(:to => @projlead.email, :subject => "New comment on (#{@comment.project.name})")
+    end
+  end
 end
