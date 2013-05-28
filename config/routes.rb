f3d806282f33985884074ce53c972691dab639f8
Brian Ridings <bdrid001@gmail.com>
2013-05-27 21:09:33 -0400

    Added Password Recovery

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Tiramisu::Application.routes.draw do
 
+  get "password_resets/create"
+
+  get "password_resets/edit"
+
+  get "password_resets/update"
+
   get 'logout'  => 'sessions#destroy', :as => 'logout'
   get 'login'   => 'sessions#new', :as => 'login'
   get 'signup'  => 'users#new', :as => 'signup'
   
   resources :users
   resources :sessions
+  resources :password_resets
   resources :projects do
     post 'comments' => 'comments#create', :as => 'comments'
     get  'comments/:id/edit' => 'comments#edit', :as => 'edit_comment'
